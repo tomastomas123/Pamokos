1c2582769fcf26c70c05290ec59a821cd0c129a4
tomastomas <tomastomazo@gmail.com>
2017-01-17 17:29:23 +0200

    Pabaigtas zaidimas

--- 2_pamoka_10_11/ShipBattle/ShipBattleTraining/Functions.cs
+++ 2_pamoka_10_11/ShipBattle/ShipBattleTraining/Functions.cs
@@ -4,6 +4,19 @@
     {
         public static ShotTarget SelectShotCoordinates(FieldData[][] battleField, ShotTarget[] previousTargets)
         {
+
+            for (int i = 0; i <10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (FieldData.StatusUnknonw == battleField[i][j])
+                    {
+                        return new ShotTarget { X = i, Y = j };
+                    }
+                   
+                }
+            }
+
             return new ShotTarget { X = 0, Y = 0 };
         }
     }
